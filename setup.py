e6148ff2fcce26630d878f328f3dc4f27a2a067f
Sophie Chen <sophiecc@umich.edu>
2023-08-04 13:12:54 -0400

    Backup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='peatmoss',
+    version='0.1',
+    packages=find_packages('py_test_pack'),
+    description='A simple peatmoss package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/sophiecchen/py_test_pack',
+)
