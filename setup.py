d3573e9d6c321b9ab76d3f1cf0152cf935b024d5
Sophie Chen <sophiecc@umich.edu>
2023-08-07 19:12:02 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='peatmoss',
     version='0.1',
-    packages=find_packages('py_test_pack'),
+    packages=find_packages('peatmoss_sentence_test'),
     description='A simple peatmoss package',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/sophiecchen/py_test_pack',
+    url='https://github.com/sophiecchen/peatmoss-sentence-test',
 )
