912249805bbff596cd0cd59570d96f262f06470a
Sophie Chen <sophiecc@umich.edu>
2023-08-07 19:08:29 -0400

    Backup

--- peatmoss_sentence_test/side.py
+++ peatmoss_sentence_test/side.py
@@ -5,6 +5,6 @@ import sentence_transformers
 
 def pure_import():
     real = "all-MiniLM-L6-v2"
-    model = SentenceTransformer(real)
+    model = sentence_transformers.SentenceTransformer(real)
 
     return 0
